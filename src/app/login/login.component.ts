b2eb68ed0157798dd2715e2becb92505348ef0c0
Ex1Kx <thenewestekigoh@gmail.com>
2021-12-05 23:05:11 -0500

    [Ex1] Added Login & 404 No Found Pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadScriptsService } from '../load-scripts.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _LoadScripts:LoadScriptsService
+  ) { 
+    _LoadScripts.Carga(["login/function"])
+  }
 
   ngOnInit(): void {
   }

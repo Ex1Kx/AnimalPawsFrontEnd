56a42abaa25e99dfc7b367f7d6a2ce471b54838c
Ex1Kx <thenewestekigoh@gmail.com>
2022-03-07 09:45:49 -0500

    [Ex1] Update The HomeScreen

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faInfo } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-news',
@@ -6,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
+  faHouse = faHouse;
+  faInfo = faInfo;
+  faNewspaper = faNewspaper;
+  faUser = faUser;
 
   constructor() { }
 

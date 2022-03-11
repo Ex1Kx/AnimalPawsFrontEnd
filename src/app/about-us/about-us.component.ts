2fc9d3d3e3493c3afef7c58c2f10ecbee202ab17
Ex1Kx <thenewestekigoh@gmail.com>
2022-03-11 15:45:39 -0500

    [Ex1] Update The Homescreen

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-us',
@@ -6,8 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
+  faHouse = faHouse;
+  faAddressCard = faAddressCard;
+  faNewspaper = faNewspaper;
+  faUser = faUser;
+  faEyeSlash = faEyeSlash;
 
-  constructor() { }
+  constructor(public translate:TranslateService) {
+    this.translate.addLangs(['es', 'en']);
+    this.translate.setDefaultLang('es');
+    this.translate.use('en');
+   }
 
   ngOnInit(): void {
   }
